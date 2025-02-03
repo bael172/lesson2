23aec290a5e848c3f96d09e790f7b310481398d0
bael <bael172@mail.ru>
2025-02-03 16:07:28 +0300

    add web server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const { canTreatArrayAsAnd } = require("sequelize/lib/utils")
+
+const app = express()
+
+PORT=8080
+
+app.get('/',(req,res)=>{
+    res.status(400).send("hello world")
+})
+const start = ()=>{
+    try{
+        app.listen(PORT,()=>console.log(`Server starts on PORT=${PORT}`))
+    }
+    catch(error){
+        console.error('Unable to listen the port',error)
+    }
+}
+start()
