e68f26ee244f331838f2cb8793f33ad25fcb2105
bael <bael172@mail.ru>
2025-02-10 12:51:16 +0300

    feat:ранжирование свйоств пользователя по степени важности

--- db/models.js
+++ db/models.js
@@ -3,18 +3,18 @@ const {DataTypes,Op} = require("sequelize")
 
 const User = sequelize.define('user',{
     id:{type:DataTypes.INTEGER, primaryKey:true, autoIncrement:true},
+    surname:{type:DataTypes.STRING}, allowNull:false,
+    name:{type:DataTypes.STRING,allowNull:false},
+    lastname:{type:DataTypes.STRING},
     passport_serial:{type:DataTypes.INTEGER},
     passport_number:{type:DataTypes.INTEGER},
     birthday:{type:DataTypes.DATEONLY},
     birthplace:{type:DataTypes.STRING},
     phone:{type:DataTypes.STRING},
     email:{type:DataTypes.STRING},
-    surname:{type:DataTypes.STRING}, allowNull:false,
-    name:{type:DataTypes.STRING,allowNull:false},
-    lastname:{type:DataTypes.STRING},
+    registr_at:{type:DataTypes.STRING},
+    lives_now:{type:DataTypes.STRING},
     tg:{type:DataTypes.STRING},
     vk:{type:DataTypes.STRING},
-    registr_at:{type:DataTypes.STRING},
-    lives_now:{type:DataTypes.STRING}
 })
 module.exports = {User}
