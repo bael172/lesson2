12e386071d129971f93a7c1ef4e4d17d23288c3f
bael <bael172@mail.ru>
2025-02-24 13:58:10 +0300

    fix: new Sequelize parameters order was fixed

--- db/db_connect.js
+++ db/db_connect.js
@@ -2,9 +2,10 @@ const Sequelize = require("sequelize")
 require("dotenv").config({path:'./env'})
 
 module.exports = new Sequelize(
+    process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
-    process.env.DB_NAME,{
+    {
         dialect:"postgres",
         host:process.env.DB_HOST,
         port:process.env.DB_PORT
